582103622b1bb70db5b8cd70f282986910632a6b
WasAlexHere <sethtorston@mail.ru>
2023-06-10 20:56:08 +0300

    added new app

---
+++ app/constants.py
@@ -0,0 +1,7 @@
+w_icon = "/Users/alexey/GIT/stndApp/app/icons/w_icon.png"
+b_icon = "/Users/alexey/GIT/stndApp/app/icons/b_icon.png"
+
+about_message = """
+A simple menu bar application for stand up timer.\n 
+You can set a timer for reminder to stand up!
+"""
